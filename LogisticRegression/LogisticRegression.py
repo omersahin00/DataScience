acf03cb237f835ecfaf682f4143f3d2a36e88aa2
Ömer Şahin <omersahin0309@gmail.com>
2024-07-10 18:14:21 +0300

    LogisticRegression added

--- LogisticRegression/LogisticRegression.py
+++ LogisticRegression/LogisticRegression.py
@@ -38,15 +38,33 @@ cm = confusion_matrix(y_test, y_pred)
 AccuracyScore = accuracy_score(y_test, y_pred) # Çıktı: 0.821917808219178
 
 #Train Set sonuçlarının görselleştirilmesi
+from matplotlib.colors import ListedColormap
+
 x_set = ss.inverse_transform(x_train)
+y_set = y_train #-> Bu değişkeni standart hale getirmemiştik. (sadece 2 adet değişkenden oluştuğu için)
+
+X1, X2 = np.meshgrid(
+    np.arange(start = x_set[:, 0].min() - 10, stop = x_set[:, 0].max() + 10, step = 0.25),
+    np.arange(start = x_set[:, 1].min() - 1000, stop = x_set[:, 1].max() + 1000, step = 0.25)
+)
+
+plt.contourf(X1, X2, classifier.predict(ss.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'blue')))
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1], c = ListedColormap(('red', 'blue'))(i), label = j)
+plt.title('Logistic Regression - Test Set')
+plt.xlabel('Yaş')
+plt.ylabel('Maaş')
+plt.legend()
+plt.show()
 
 
-
-
-
-
-
-
+# Bu model linear'dir. Yani doğrusal bir öğrenim yapar.
+# Verileri bir tabloda gösterdiğimizde ortada çıkan çizgi tahmin ayrım çizgisidir.
+# Tahmin verilerini node'ların bu çizginin hangi tarafında bulunduğunda göre karar verir.
 
 
 
