a4c0618b4201803d2a9b457c2f085b3c5ccb7f18
Ömer Şahin <omersahin0309@gmail.com>
2024-07-12 18:32:28 +0300

    Update SupportVectorMachine.py

--- SupportVectorMachine/SupportVectorMachine.py
+++ SupportVectorMachine/SupportVectorMachine.py
@@ -20,7 +20,7 @@ x_test = ss.transform(x_test)
 
 #SVM modelinin kurulması
 from sklearn.svm import SVC
-classifier = SVC()
+classifier = SVC(probability=True, kernel="rbf")
 
 
 #Modelin eğitilmesi
@@ -34,3 +34,35 @@ SVMTahmin = classifier.predict(ss.transform([[25, 150000]])) #-> Çıktı: 1
 #Test set üzerinde sınama
 y_pred = classifier.predict(x_test)
 
+SVMProbabiliy = classifier.predict_proba(x_test)
+
+
+#Confusion Matrix ve Accuracy Score
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+AccuracySVM = accuracy_score(y_test, y_pred)
+
+"""
+#Görselleştirme
+from matplotlib.colors import ListedColormap
+x_set = ss.inverse_transform(x_train)
+y_set = y_train
+
+X1, X2 = np.meshgrid(np.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step = 0.25),
+                     np.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.25))
+
+
+plt.contourf(X1, X2, classifier.predict(ss.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'blue')))
+
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1], c = ListedColormap(('red', 'blue'))(i), label = j)
+plt.title('Support Vector Machine - Train Set (Linear)')
+plt.xlabel('Yaş')
+plt.ylabel('Maaş')
+plt.legend()
+plt.show()
+"""
