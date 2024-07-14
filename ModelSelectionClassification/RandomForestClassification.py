ab769003dcb05b391b366a31746266aee11cfb06
Ömer Şahin <omersahin0309@gmail.com>
2024-07-14 21:24:06 +0300

    DecisionTreeClassification added

---
+++ ModelSelectionClassification/RandomForestClassification.py
@@ -0,0 +1,1 @@
+import pandas as pdimport numpy as npimport matplotlib.pyplot as pltdataset = pd.read_csv("breast_cancer.csv")x = dataset.iloc[:, 1:-1].valuesy = dataset.iloc[:,-1].valuesfrom sklearn.model_selection import train_test_splitx_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=1)
