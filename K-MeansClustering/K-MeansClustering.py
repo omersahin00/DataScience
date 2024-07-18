4b646430065616a7047936f98adc3ae212953ff5
Ömer Şahin <omersahin0309@gmail.com>
2024-07-18 11:56:42 +0300

    K-MeansClustering transactions have been completed.

--- K-MeansClustering/K-MeansClustering.py
+++ K-MeansClustering/K-MeansClustering.py
@@ -36,3 +36,24 @@ plt.show()
 #Optimum cluster sayısıyla K-Means modelinin kurulması
 kmeans = KMeans(n_clusters = 5, init = "k-means++", random_state = 0)
 y_kmeans = kmeans.fit_predict(x)
+clusters_kordinate = kmeans.cluster_centers_
+
+
+#Clusterların Plot Olarak Çizilmesi
+plt.scatter(x[y_kmeans == 0, 0], x[y_kmeans == 0, 1], s = 100, c = "blue", label = "Class 1")
+plt.scatter(x[y_kmeans == 1, 0], x[y_kmeans == 1, 1], s = 100, c = "green", label = "Class 2")
+plt.scatter(x[y_kmeans == 2, 0], x[y_kmeans == 2, 1], s = 100, c = "red", label = "Class 3")
+plt.scatter(x[y_kmeans == 3, 0], x[y_kmeans == 3, 1], s = 100, c = "purple", label = "Class 4")
+plt.scatter(x[y_kmeans == 4, 0], x[y_kmeans == 4, 1], s = 100, c = "yellow", label = "Class 5")
+plt.scatter(kmeans.cluster_centers_[:, 0], kmeans.cluster_centers_[:, 1], s = 300, c = "black", label = "Centroids")
+plt.legend() #-> Grafiğe noktaların ne anlama geldiğini gösteren bir tablo ekler.
+plt.title("Müşteri Segmentasyonu")
+plt.xlabel("Yıllık Gelir")
+plt.ylabel("Harcama Skoru")
+plt.show()
+
+
+
+
+
+
