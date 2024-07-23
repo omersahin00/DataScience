9e9eb2969cbf9f3280f0c8efcf120c8ed643e533
Ömer Şahin <omersahin0309@gmail.com>
2024-07-23 17:16:31 +0300

    Pedict added.

--- NaturalLanguagueProcessing/NaturalLanguagueProcessing.py
+++ NaturalLanguagueProcessing/NaturalLanguagueProcessing.py
@@ -46,3 +46,46 @@ for i in range(0, 1000):
         temp_list.append(ps.stem(word))
     dataset["Review"][i] = temp_list
 
+
+#Cümleleri geri birleştirme
+for i in range(0, 1000):
+    dataset["Review"][i] = " ".join(dataset["Review"][i])
+
+
+#Bag of Words
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+
+x = cv.fit_transform(dataset["Review"]).toarray()
+y = dataset.iloc[:, -1].values
+
+
+#Train ve Test Setleri
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+
+
+#Naive Bayes Modelinin Kurulması
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+
+classifier.fit(x_train, y_train)
+
+y_pred = classifier.predict(x_test)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+cm = confusion_matrix(y_test, y_pred)
+AccuracyScoreNB = accuracy_score(y_test, y_pred)
+
+
+
+
+
+
+
+
+
+
+
+
